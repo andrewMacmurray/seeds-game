57754ffb35825814ae304157bbe79546311d1e32
Andrew MacMurray <a.macmurray@icloud.com>
2018-03-15 18:08:41 +0000

    fix momentum scroll bug on hub

--- src/elm/Scenes/Hub/View.elm
+++ src/elm/Scenes/Hub/View.elm
@@ -14,30 +14,25 @@ import Views.Lives exposing (livesLeft)
 
 hubView : Model -> Html Msg
 hubView model =
-    div
-        [ class "w-100 fixed overflow-y-scroll momentum-scroll z-2"
-        , id "hub"
-        , style [ heightStyle model.window.height ]
-        , handleHideInfo model
+    div [ handleHideInfo model ]
+        [ hubTopBar model
+        , info model
+        , div
+            [ class "w-100 fixed overflow-y-scroll momentum-scroll z-2"
+            , id "hub"
+            , style [ heightStyle model.window.height ]
+            ]
+            (renderWorlds model)
         ]
-        (hubContent model)
-
-
-hubContent : Model -> List (Html Msg)
-hubContent model =
-    [ info model
-    , hubTopBar model
-    ]
-        ++ renderWorlds model
 
 
 hubTopBar : Model -> Html msg
 hubTopBar model =
     div
-        [ class "fixed w-100 top-0 tc pa2"
+        [ class "w-100 fixed z-3 top-0 tc pa1 pa2-ns"
         , style [ background washedYellow ]
         ]
-        [ div [ style [ transformStyle <| scale 0.7 ] ] <| livesLeft model.lives
+        [ div [ style [ transformStyle <| scale 0.5 ] ] <| livesLeft model.lives
         , div [ class "f7", style [ color darkYellow ] ] [ renderCountDown model.timeTillNextLife ]
         ]
 
@@ -46,12 +41,12 @@ renderCountDown : Time -> Html msg
 renderCountDown timeRemaining =
     case timeLeft timeRemaining of
         Nothing ->
-            p [ class "ma1" ] [ text "full life" ]
+            p [ class "ma1 mt0" ] [ text "full life" ]
 
         Just t ->
             div []
-                [ p [ class "dib ma1" ] [ text "Next life in: " ]
-                , p [ style [ color pinkRed ], class "dib ma1" ] [ text <| renderTime t ]
+                [ p [ style [ marginTop -2 ], class "dib ma1 mt0" ] [ text "Next life in: " ]
+                , p [ style [ color pinkRed ], class "dib ma1 mt0" ] [ text <| renderTime t ]
                 ]
 
 
