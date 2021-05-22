64db24b599634fd6d117fcd76c6b444fd7610af3
andrewMacmurray <a.macmurray@icloud.com>
2021-05-22 09:31:16 +0100

    make circles from axis on center point

--- src/elm/Scene/Garden/Chrysanthemum.elm
+++ src/elm/Scene/Garden/Chrysanthemum.elm
@@ -1,10 +1,12 @@
 module Scene.Garden.Chrysanthemum exposing (hills)
 
+import Axis2d exposing (Axis2d)
 import Circle2d exposing (Circle2d)
+import Direction2d
 import Element exposing (Color)
 import Element.Palette as Palette
 import Pixels exposing (Pixels)
-import Point2d
+import Point2d exposing (Point2d)
 import Scene.Garden.Shape as Shape exposing (Shape)
 import Utils.Geometry exposing (down)
 import Window exposing (Window, vh, vw)
@@ -12,15 +14,17 @@ import Window exposing (Window, vh, vw)
 
 hills : Window -> Shape
 hills window =
-    Shape.moveDown 50
+    Shape.moveDown (Window.whenNarrow 130 80 window)
         (Shape.group
-            [ middleHill -250 window Palette.purple8
+            [ roundHill -200 Palette.purple10 window
+            , Shape.mirror (roundHill -200 Palette.purple7 window)
+            , middleHill -250 window Palette.purple8
             , roundHill 0 Palette.purple3 window
             , Shape.mirror (roundHill 0 Palette.purple2 window)
             , middleHill -100 window Palette.purple1
             , roundHill 300 Palette.purple9 window
             , Shape.mirror (roundHill 300 Palette.purple7 window)
-            , middleHill 310 window Palette.purple3
+            , middleHill 250 window Palette.purple3
             ]
         )
 
@@ -48,10 +52,22 @@ roundHill_ : Float -> Window -> Circle2d Pixels coordinates
 roundHill_ y w =
     let
         r =
-            clamp 400 1800 (vw w - 400)
+            clamp 400 1800 (vw w / 2)
     in
     Circle2d.translateBy (down y)
         (Circle2d.atPoint
-            (Point2d.pixels (vw w / 15) r)
+            (point w)
             (Pixels.pixels r)
         )
+
+
+point : Window -> Point2d Pixels coordinates
+point w =
+    Point2d.along (axis w) (Pixels.pixels (vw w / 2))
+
+
+axis : Window -> Axis2d Pixels coordinates
+axis w =
+    Axis2d.withDirection
+        (Direction2d.degrees (vw w / 50))
+        (Point2d.pixels (vw w / 2) (vh w / 2))
