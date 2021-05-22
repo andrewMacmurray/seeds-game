769ca69e29fd05fd9a1c9d841945cb834ff0c5e9
andrewMacmurray <a.macmurray@icloud.com>
2021-05-22 15:17:58 +0100

    remove unused animation lib

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import "pepjs";
-import "@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js";
-import "@webcomponents/webcomponentsjs/webcomponents-loader";
-import "./ts/animation";
 import * as Cheats from "./ts/cheats";
 import * as App from "./ts/app";
 
